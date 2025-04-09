bd4d1700cf199e4351d849dbebaa80ddb52cc30a
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-04-09 02:32:19 -0400

    flask app NONFUNCTIONAL

--- backend/app.py
+++ backend/app.py
@@ -3,8 +3,9 @@ from flask_cors import CORS
 import os 
 from dotenv import load_dotenv
 import psycopg2
+from scraper import scraper
 load_dotenv()
-
+# load env 
 
 
 app = Flask(__name__)
@@ -14,11 +15,13 @@ CORS(app)
 def hello():
     return jsonify(message="Hello, World!")
 
+@app.route('/api/scrape', methods=['GET','POST'])
+def scrape():
+    if request.method == 'GET':
+        scraped_data = scraper()
+        return jsonify(scraped_data)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
